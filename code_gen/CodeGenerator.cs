d1a9f3599efc6d66eb7672781ca7e2e15f643aa4
Amir Aminov <amir.aminov.18@mail.ru>
2025-11-15 21:30:48 +0300

    Add structure for types in cil

--- code_gen/CodeGenerator.cs
+++ code_gen/CodeGenerator.cs
@@ -1,3 +1,4 @@
+using ImperativeLang.SemanticalAnalyzerNS;
 using ImperativeLang.SyntaxAnalyzer;
 
 namespace ImperativeLang.CodeGen
@@ -13,33 +14,68 @@ namespace ImperativeLang.CodeGen
             using (_writer)
             {
                 WriteHeader();
-                WriteProgramShell();
+
             }
         }
 
+
         private void WriteHeader()
         {
+            _writer.WriteLine(".assembly extern mscorlib {}");
             _writer.WriteLine(".assembly ImperativeLangProgram {}");
             _writer.WriteLine(".module ImperativeLangProgram.exe");
             _writer.WriteLine();
         }
 
-        private void WriteProgramShell()
+        
+        //Type declarations
+        private void GenerateTypeClasses(ProgramNode AST)
         {
-            _writer.WriteLine(".class public Program");
+            //TODO: Make a full pass with context tracking
+        }
+
+        private void GenerateArrayTypeClass(ArrayTypeInfo type, string context, int id, string? objectName = null)
+        {
+            _writer.WriteLine($".class public auto valueType {type.Name}@{context}@{id}");
             _writer.WriteLine("{");
+            string elementType = "";
+            bool flag = true;
+            if (objectName != null)
+            {
+                elementType = objectName;
+                flag = false;
+            }
+            else if (type.ElementType is PrimitiveTypeInfo primitiveTypeInfo)
+            {
+                if (primitiveTypeInfo.Type == PrimitiveType.Integer 
+                || primitiveTypeInfo.Type == PrimitiveType.Boolean) elementType = "int32";
+                if (primitiveTypeInfo.Type == PrimitiveType.Real) elementType = "float32";
+            } else
+            {
+                throw new Exception("Something went wrong ;3");
+            }
+            _writer.WriteLine($".field public {elementType}[] data");
 
-            _writer.WriteLine("    .method public static void main() cil managed");
-            _writer.WriteLine("    {");
-            _writer.WriteLine("        .entrypoint");
-
-            _writer.WriteLine("        ldstr \"Hello World!\""); //Placeholder just to see that it works.
-            _writer.WriteLine("        call void [System.Console]System.Console::WriteLine(string)");
-
-            _writer.WriteLine("        ret");
-            _writer.WriteLine("    }");
-
+            //Constructor
+            _writer.WriteLine(".method public hidebysig specialname rtspecialname instance void .ctor() cil managed");
+            _writer.WriteLine("{");
+            _writer.WriteLine(".maxstack 2");
+            _writer.WriteLine("ldarg.0");
+            _writer.WriteLine($"ldc.i4.s {type.Size}");
+            _writer.WriteLine($"newarr {(
+                flag 
+                ? $"[mscorlib]System.{char.ToUpper(elementType[0]) + elementType.Substring(1)}" 
+                : $"{elementType}")}");
+            _writer.WriteLine($"stfld {elementType}[] {type.Name}@{context}@{id}::data");
+            _writer.WriteLine("ret");
             _writer.WriteLine("}");
+
+            //TODO: Add getter (optional)
+            //TODO: Add setter (optional)
+        }
+        private void GenerateRecordTypeClass(RecordTypeInfo type, string context, int id)
+        {
+            //TODO: Implement method
         }
     }
 }
