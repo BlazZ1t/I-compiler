78f62839fdf8da081ca8e27b03b32c2328f05f1c
BlazZ1t <klimspy@gmail.com>
2025-11-16 18:55:48 +0300

    Add getter and setter for array class method

--- code_gen/CodeGenerator.cs
+++ code_gen/CodeGenerator.cs
@@ -36,42 +36,73 @@ namespace ImperativeLang.CodeGen
 
         private void GenerateArrayTypeClass(ArrayTypeInfo type, string context, int id, string? objectName = null)
         {
-            _writer.WriteLine($".class public auto valueType {type.Name}@{context}@{id}");
+            string className = $"{type.Name}@{context}@{id}";
+            _writer.WriteLine($".class public auto valuetype {className}");
             _writer.WriteLine("{");
-            string elementType = "";
-            bool flag = true;
+            string ilFieldType = "";
+            string ilNewArrType = "";
+
             if (objectName != null)
             {
-                elementType = objectName;
-                flag = false;
+                ilFieldType = objectName;
+                ilNewArrType = objectName;
             }
-            else if (type.ElementType is PrimitiveTypeInfo primitiveTypeInfo)
+            else if (type.ElementType is PrimitiveTypeInfo p)
             {
-                if (primitiveTypeInfo.Type == PrimitiveType.Integer 
-                || primitiveTypeInfo.Type == PrimitiveType.Boolean) elementType = "int32";
-                if (primitiveTypeInfo.Type == PrimitiveType.Real) elementType = "float32";
-            } else
-            {
-                throw new Exception("Something went wrong ;3");
+                switch (p.Type)
+                {
+                    case PrimitiveType.Integer:
+                    case PrimitiveType.Boolean:
+                        ilFieldType = "int32";
+                        ilNewArrType = "[mscorlib]System.Int32";
+                        break;
+
+                    case PrimitiveType.Real:
+                        ilFieldType = "float32";
+                        ilNewArrType = "[mscorlib]System.Single";
+                        break;
+                }
             }
-            _writer.WriteLine($".field public {elementType}[] data");
+            else throw new Exception("Unhandled element type!");
+            _writer.WriteLine($".field public {ilFieldType}[] data");
 
             //Constructor
             _writer.WriteLine(".method public hidebysig specialname rtspecialname instance void .ctor() cil managed");
             _writer.WriteLine("{");
-            _writer.WriteLine(".maxstack 2");
+            _writer.WriteLine(".maxstack 3");
+            _writer.WriteLine("ldarg.0");
+            _writer.WriteLine("call instance void [mscorlib]System.ValueType::.ctor()");
             _writer.WriteLine("ldarg.0");
             _writer.WriteLine($"ldc.i4.s {type.Size}");
-            _writer.WriteLine($"newarr {(
-                flag 
-                ? $"[mscorlib]System.{char.ToUpper(elementType[0]) + elementType.Substring(1)}" 
-                : $"{elementType}")}");
-            _writer.WriteLine($"stfld {elementType}[] {type.Name}@{context}@{id}::data");
+            _writer.WriteLine($"newarr {ilNewArrType}");
+            _writer.WriteLine($"stfld {ilFieldType}[] {className}::data");
             _writer.WriteLine("ret");
             _writer.WriteLine("}");
 
-            //TODO: Add getter (optional)
-            //TODO: Add setter (optional)
+            //Getter
+            _writer.WriteLine(".method public hidebysig instance " + ilFieldType + " get_Item(int32 index) cil managed");
+            _writer.WriteLine("{");
+            _writer.WriteLine(".maxstack 3");
+            _writer.WriteLine("ldarg.0");
+            _writer.WriteLine($"ldfld {ilFieldType}[] {className}::data");
+            _writer.WriteLine("ldarg.1");
+            _writer.WriteLine($"ldelem.{ilFieldType}");
+            _writer.WriteLine("ret");
+            _writer.WriteLine("}");
+            
+            //Setter
+            _writer.WriteLine(".method public hidebysig instance void set_Item(int32 index, " + ilFieldType + " value) cil managed");
+            _writer.WriteLine("{");
+            _writer.WriteLine(".maxstack 4");
+            _writer.WriteLine("ldarg.0");
+            _writer.WriteLine($"ldfld {ilFieldType}[] {className}::data");
+            _writer.WriteLine("ldarg.1");
+            _writer.WriteLine("ldarg.2");
+            _writer.WriteLine($"stelem.{ilFieldType}");
+            _writer.WriteLine("ret");
+            _writer.WriteLine("}");
+
+            _writer.WriteLine("}");
         }
         private void GenerateRecordTypeClass(RecordTypeInfo type, string context, int id)
         {
